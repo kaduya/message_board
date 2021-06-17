b43b9f4f9b105d2ac72dfb0543d30efd6641195c
kaduya <kaduya.t@gmail.com>
2021-06-17 18:32:00 +0900

    Add flush message

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -17,8 +17,8 @@ import utils.DBUtil;
  */
 @WebServlet("/destroy")
 public class DestroyServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,28 +26,29 @@ public class DestroyServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String _token = request.getParameter("_token");
-		if(_token != null && _token.equals(request.getSession().getId())) {
-			EntityManager em = DBUtil.createEntityManager();
-			
-			// セッションスコープからメッセージのIDを取得して
-			// 該当のIDのメッセージ1件のみをデータベースから取得
-			Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
-			
-			em.getTransaction().begin();
-			em.remove(m);
-			em.getTransaction().commit();
-			em.close();
-			
-			// セッションスコープ上の不要になったデータを削除
-			request.getSession().removeAttribute("message_id");
-			
-			// indexページへリダイレクト
-			response.sendRedirect(request.getContextPath() + "/index");
-		}
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+
+            em.getTransaction().begin();
+            em.remove(m);
+            em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "登録が完了しました。");
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("message_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
 }
